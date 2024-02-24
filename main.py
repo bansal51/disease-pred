cd615bea6aabce7cab5fd8a9388c019b7cdc0bc2
unknown <yashyog2012@gmail.com>
2024-02-23 19:06:41 -0600

    split data into test and training sets

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ from sklearn.metrics import accuracy_score, confusion_matrix
 training_data_path = "dataset/Training.csv"
 # Drop the ending column --> empty column
 training_data = pd.read_csv(training_data_path).dropna(axis=1)
+print(training_data)
 
 # Check to see whether the dataset is balanced
 disease_count = training_data["prognosis"].value_counts()
@@ -30,3 +31,10 @@ plt.xticks(rotation=90)
 # Label Encoder assigns a numerical value to the various prognoses --> if there are n labals, the labels will be assigned from 0 to n - 1
 encoder = LabelEncoder()
 training_data["prognosis"] = encoder.fit_transform(training_data["prognosis"])
+
+# The X group should be all the columns used to determine the prognosis (independent variables)
+# The y group should be the columns which is the prognosis (dependent variable)
+# Test data is only 42 entries, we want a 80/20 split between the training and test data --> we will split the training data
+X = training_data.iloc[:, :-1]
+y = training_data.iloc[:, -1]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
